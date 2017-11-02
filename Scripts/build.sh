32fc1fdf5fd80038b202937b1c3d991a2da65509
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2017-11-02 15:30:16 +0000

    fixing tests

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Build SpecTools"
-cd ./Example/ && set -o pipefail && xcodebuild test -workspace SpecTools.xcworkspace -scheme SpecTools-Example -destination 'platform=iOS Simulator,OS=11.0,name=iPhone X'
+cd ./Example/ && set -o pipefail && xcodebuild test -workspace SpecTools.xcworkspace -scheme SpecTools-Example -destination 'platform=iOS Simulator,name=iPhone X'
 
